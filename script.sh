e81b8594243711fbe830170e10807d3a025588d9
lucaster <luca.cavagnoli@gmail.com>
2021-03-20 19:07:46 +0100

    curl

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ docker build -t lucaster/udemy-academind-docker-module09-04-backend ./backend
 docker push lucaster/udemy-academind-docker-module09-04-backend
 
 
-18.208.195.218
+curl http://goals-lb-1404692755.us-east-1.elb.amazonaws.com/goals
