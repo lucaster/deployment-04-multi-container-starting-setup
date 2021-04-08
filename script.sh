4df7a11fd12450b1a5658c1a9640eef2317e877c
lucaster <luca.cavagnoli@gmail.com>
2021-04-08 21:12:03 +0200

    trying to connect to mongodb atlas

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 docker build -t lucaster/udemy-academind-docker-module09-04-backend ./backend
 docker push lucaster/udemy-academind-docker-module09-04-backend
 
+docker run --rm --env-file env/backend.env lucaster/udemy-academind-docker-module09-04-backend
 
 curl http://goals-lb-1404692755.us-east-1.elb.amazonaws.com/goals
+curl http://34.226.246.97/goals
